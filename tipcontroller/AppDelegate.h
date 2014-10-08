39c6a802051fac548371cb5712a257ba02cfbd0b
Sumeet Ungratwar <sumeet@fb.com>
2014-10-08 08:56:05 -0700

    Initial Commit

---
+++ tipcontroller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipcontroller
+//
+//  Created by Sumeet Ungratwar on 10/8/14.
+//  Copyright (c) 2014 Sumeet Ungratwar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipcontroller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipcontroller
+//
+//  Created by Sumeet Ungratwar on 10/8/14.
+//  Copyright (c) 2014 Sumeet Ungratwar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
